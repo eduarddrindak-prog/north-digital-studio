fc9c5e25d8788bb983e1c872fe3f145c46938ff0
Eduard <eduarddrindak@gmail.com>
2026-08-05 14:20:34 +0200

    1.5.11 debug

--- src/pages/Contact/ContactForm/Form.tsx
+++ src/pages/Contact/ContactForm/Form.tsx
@@ -1,4 +1,4 @@
-import Button from "@/components/ui/Button";
+
 import Input from "@/components/ui/Input";
 import Textarea from "@/components/ui/Textarea";
 import Reveal from "@/components/common/Reveal";
